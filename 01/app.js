d90355ce948aa35c1db7ec60788cbea036402c34
jeandrei <jeandreiwalter@gmail.com>
2022-08-16 19:36:16 +0000

    parte do html

---
+++ 01/app.js
@@ -0,0 +1,18 @@
+/**
+ * app.js irá conter tudo, todos os models, controllers ,localstorage
+ * Iremos usar o framework materialize css
+ * https://materializecss.com/
+ * 
+ * Também o font awesome
+ * <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+ * 
+ * Também precisa do jquery 3 minified version
+ * Tem que vir antes do materialize
+ * https://releases.jquery.com/
+ * 
+ * Criamos o navbar
+ * Criamos o botão clear
+ * Criamos o formulário
+ * 
+ */
